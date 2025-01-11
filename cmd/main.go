223628e1a1c1a1d3309dd1c8d7d8768c906edc88
mar4ehk0 <mar4ehk0@gmail.com>
2025-01-11 18:38:36 +0300

    added viper

--- cmd/main.go
+++ cmd/main.go
@@ -6,14 +6,29 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/mar4ehk0/notes"
 	"github.com/mar4ehk0/notes/pkg/handler"
+	"github.com/mar4ehk0/notes/pkg/repository"
+	"github.com/mar4ehk0/notes/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	router := gin.Default()
-	handler := handler.New(router)
+	repository := repository.NewRepository()
+	service := service.NewService(repository)
+	handler := handler.New(router, service)
 
 	srv := notes.Server{}
-	if err := srv.Run("8080", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
