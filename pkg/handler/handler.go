f9ba61bd626e9edc8aadb56bf9eb2d683deec380
mar4ehk0 <mar4ehk0@gmail.com>
2025-01-12 14:56:36 +0300

    chnaged

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,19 +1,23 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mar4ehk0/notes/pkg/service"
+)
 
 type Handler struct {
-	router *gin.Engine
+	router  *gin.Engine
+	service *service.Service
 }
 
-func New(router *gin.Engine) *Handler {
+func New(router *gin.Engine, service *service.Service) *Handler {
 	router.LoadHTMLGlob("templates/**/*")
 
 	router.Static("/img", "./static/img")
 	router.Static("/css", "./static/css")
 	router.StaticFile("/favicon.ico", "./static/img/favicon.ico")
 
-	return &Handler{router: router}
+	return &Handler{router: router, service: service}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
