276ed4be37250b7f9957dd452fb8da23e0f725e0
mar4ehk0 <mar4ehk0@gmail.com>
2025-01-24 20:19:24 +0300

    added Is to ForbiddenError

--- pkg/service/error.go
+++ pkg/service/error.go
@@ -14,3 +14,11 @@ func NewForbiddenError(userID int, entityID int) *ForbiddenError {
 func (err ForbiddenError) Error() string {
 	return fmt.Sprintf("user id %d access denied to note id %d", err.UserID, err.EntityID)
 }
+
+func (err ForbiddenError) Is(target error) bool {
+	t, ok := target.(*ForbiddenError)
+	if !ok {
+		return false
+	}
+	return err.UserID == t.UserID && err.EntityID == t.EntityID
+}
